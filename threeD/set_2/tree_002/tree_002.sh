d25a3dd23522cea252fd202f4202fe4b365fe675
natalia-rubio <natalialeonie72@gmail.com>
2025-11-13 09:57:55 -0800

    first commit

---
+++ threeD/set_2/tree_002/tree_002.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+#SBATCH --job-name=tree_002
+#SBATCH --partition=amarsden
+#SBATCH --output=/scratch/users/nrubio/job_scripts/tree_002.o%j
+#SBATCH --error=/scratch/users/nrubio/job_scripts/tree_002.e%j
+#SBATCH --time=3:00:00
+#SBATCH --mem=50000
+#SBATCH --nodes=2
+#SBATCH --tasks-per-node=24
+
+export UCX_TLS=ib
+export PMIX_MCA_gds=hash
+export OMPI_MCA_btl_tcp_if_include=ib0
+export F1='/scratch/users/nrubio/synthetic_junctions/CCO_trees/set_2/tree_002'
+export IMAGE_PATH='/home/users/nrubio/SV_scripts/solver_latest.sif'
+
+# Load Modules
+module purge
+module load openmpi
+# Name of the executable you want to run
+mpirun --mca mpi_cuda_support 0 -n 48 singularity run $IMAGE_PATH /home/users/nrubio/svMultiPhysics/build/svMultiPhysics-build/bin/svmultiphysics $F1/fluid_simulation_0-0.xml
